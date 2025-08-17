ac52d2d0675d6a300af5b8496db54a7421f69ae5
Arun Purohit <arunpurohit234@gmail.com>
2025-08-17 22:51:40 +0530

    study notion react practice project

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div className='flex justify-center h-full items-center text-white text-3xl'>Welcome to study notion</div>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='flex justify-center h-full items-center text-white text-3xl'>Home</div>
+  )
+}
